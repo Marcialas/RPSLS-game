4dbc0a9d19412bd1ef5e7c698fda31f584bee2d8
Marcialas <martynaspalsys@gmail.com>
2022-10-20 16:18:11 +0000

    Add logic to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,71 @@
-/**Declare constants for DOM 
- * and possible choices
-*/
 
-const options = document.getElementById('options-item');
-const playerScore = document.getElementsByClassName('player-score');
-const computerScore = document.getElementsByClassName('computer-score');
-const playerChoice = document.getElementsByClassName('player-choice');
-const computerChoice = document.getElementsByClassName('computer-choice');
-const message = document.getElementById('message');
-const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const message = document.getElementById("message");
 
-/** Add event listeners for all options */
-
-for (let option of options) {
-    option.addEventListener('click', function() {
-        let playerChoice = this.getAttribute('data-choice');
-        playGame(playerChoice);
-    });
+let gameRules = {
+    Rock: {
+        Rock: 'draw',
+        Scissors: 'lose',
+        Paper: 'win',
+        Spock: 'win',
+        Lizard: 'lose'
+    },
+    Paper: {
+        Paper: 'draw',
+        Rock: 'lose',
+        Scissors: 'win',
+        Lizard: 'win',
+        Spock: 'lose'
+    },
+    Scissors: {
+        Scissors: 'draw',
+        Paper: 'lose',
+        Rock: 'win',
+        Lizard: 'lose',
+        Spock: 'win'
+    },
+    Lizard: {
+        Scissors: 'win',
+        Paper: 'lose',
+        Rock: 'win',
+        Lizard: 'draw',
+        Spock: 'lose'
+    },
+    Spock: {
+        Scissors: 'lose',
+        Paper: 'win',
+        Rock: 'lose',
+        Lizard: 'win',
+        Spock: 'draw'
+    }
 }
+
+function clicked(input) {
+    let choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+    let randomNumber = Math.floor(Math.random() * 5); 
+    let comp_choice = choices[randomNumber];
+
+
+    document.getElementById("compChoice").innerHTML = `Computer choose <span>${comp_choice.toUpperCase()}</span>`;  
+    document.getElementById("userChoice").innerHTML = `You choose <span>${input.toUpperCase()}</span>`;
+
+    
+
+	console.log('input', input, 'comp- choice', comp_choice);
+    switch(gameRules[comp_choice][input]) {
+        case 'win':
+            message.innerText = `You win`;
+            message.style.cssText = "background-color: rgb(128, 247, 128)";
+            user_score++;
+            break;
+        case 'lose':
+            message.innerText = `You lose`;
+            message.style.cssText = "background-color: rgb(240, 124, 124)";
+            comp_score++;
+            break;
+        case 'draw':
+            message.innerText = `You draw`;
+            message.style.cssText = "background-color: rgb(102, 102, 102)";
+            break;   
+    }
+}
+
